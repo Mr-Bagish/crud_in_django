89cb29da2bc09e3f42bcf134a5f45c6d60dadb2d
Mr-Bagish <bagishgautam4@gmail.com>
2024-12-07 22:05:57 +0545

    Host update

--- tweeter_v2/settings.py
+++ tweeter_v2/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1!l5qkp)k11l+%5d6zw8gzqje1x@qb9%c8elnztgxat86yjtlq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
